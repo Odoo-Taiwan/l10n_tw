c6cf7274cb9ff6036293b64220777ecad9d37d34
micktseng <mick@truney.com>
2020-10-08 20:16:38 +0800

    初始化

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 Mick Tseng suncombo@gmail.com
+
+{
+    'name': '台灣本地化',
+    'version': '1.0',
+    'category': 'Localization',
+    'description': """ 針對台灣做本地化資料匯入，包含 縣市、銀行等等 """,
+    'author': 'Mick Tseng',
+    'depends': [],
+    'data': [
+        'data/res_country_state_data.xml',
+        'data/res_currency_data.xml',
+        'data/res.bank.csv',
+    ],
+}
