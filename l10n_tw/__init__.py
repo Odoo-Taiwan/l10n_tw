a293e2affe5018f4dff5e7f28de4f08e187fde24
micktseng <mick@truney.com>
2020-12-03 14:58:39 +0800

    修正 chrome 不支援 odoo 的台灣中文語系

--- l10n_tw/__init__.py
+++ l10n_tw/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2020 Mick Tseng, Odoo Taiwan - suncombo@gmail.com
 
+import babel.core
+
+babel.core.LOCALE_ALIASES['zh'] = 'zh_TW'
+
 
 def uninstall_hook(cr, registry):
     cr.execute(
